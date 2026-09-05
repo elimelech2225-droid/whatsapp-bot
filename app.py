c9f190b0d90b36b8dfb20c027097f0e621c71f57
elimelech2225-droid <elimelech.22.25@gmail.com>
2026-09-05 21:55:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1589,7 +1589,7 @@ def handle_registration(phone, text, action_id):
         )
         return True
 
-     if state == "driver_vehicle_number":
+         if state == "driver_vehicle_number":
         number = (
             ""
             if text.strip() == "אין"
@@ -1637,8 +1637,7 @@ def handle_registration(phone, text, action_id):
                 ),
             ]
         )
-        return True   
-
+        return True
     
 
 # =========================================================
