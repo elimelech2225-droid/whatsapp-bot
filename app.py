bdcb07fb1fa367ab628e1dbecdc6a36781345b2b
elimelech2225-droid <elimelech.22.25@gmail.com>
2026-09-13 18:33:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -451,8 +451,8 @@ def normalize_phone(value):
 
 
 def get_user(phone):
-        phone = normalize_phone(phone)
-        with db() as conn:
+    phone = normalize_phone(phone)
+    with db() as conn:
         row = conn.execute(
             """
             SELECT *
