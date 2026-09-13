a66848c03049bacfde62f35263e0f3bd4fc4840d
elimelech2225-droid <elimelech.22.25@gmail.com>
2026-09-13 15:45:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -429,7 +429,9 @@ def init_db():
         except sqlite3.OperationalError:
             pass
 init_db()
-
+with db() as conn:
+    conn.execute("DELETE FROM users")
+    conn.commit()
 # =========================================================
 # כלי עזר
 # =========================================================
