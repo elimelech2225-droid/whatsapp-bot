32fe95b8d225313cb50103ab41486de9b8972eb2
elimelech2225-droid <elimelech.22.25@gmail.com>
2026-09-13 19:17:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5329,12 +5329,13 @@ def webhook():
         )
 
         if not user:
-            if handle_registration(
-                phone,
-                text,
-                action_id
-            ):
-                return "ok", 200
+    handle_registration(
+        phone,
+        text,
+        action_id
+    )
+    return "ok", 200
+             
             
 
         
