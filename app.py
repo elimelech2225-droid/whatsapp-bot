3c45b78817d5ce4f74a683defd87a8e8ebcbfad1
elimelech2225-droid <elimelech.22.25@gmail.com>
2026-09-19 18:55:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4161,7 +4161,7 @@ def handle_admin(
 
         return True
         if action_id == "admin_dispatchers":
-        send_buttons(
+            send_buttons(
             phone,
             "👥 ניהול סדרנים",
             [
